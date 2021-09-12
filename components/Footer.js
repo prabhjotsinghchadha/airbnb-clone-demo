8563e007962b003d76e24c24ba772640cef6cc2d
prabhjotsinghchadha <prabhjottechs@gmail.com>
2021-09-12 19:47:21 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,38 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awesome clone</p>
+        <p>It's open source</p>
+        <p>Download code from github</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p>Prabhjot Singh Chadha</p>
+        <p>Presents</p>
+        <p>AirBNB Clone</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi Prabhjot</p>
+        <p>prabhjottechs@gmail.com</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
